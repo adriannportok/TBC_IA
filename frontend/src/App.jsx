357f111c2bfee15438bf8991d74b89a76c7a6535
adriannportok <pink21guy@gmail.com>
2025-08-31 20:56:51 -0500

    Design Login no backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import fondo from "./assets/perfecta.jpg";
+import logo from "./assets/dr.png";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div
+        className="flex min-h-screen flex-col justify-center px-6 py-12 lg:px-8 bg-cover bg-center"
+        style={{ backgroundImage: `url(${fondo})` }}
+      >
+
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm bg-white/80 p-6 rounded-xl shadow-lg">
+        <img 
+            src={logo}
+            alt="Your Company" 
+            className="mx-auto h-25 w-auto" 
+          />
+          <h2 className="mb-10 text-center text-2xl font-bold tracking-tight text-gray-900">
+            Bienvenido
+          </h2>
+          <form action="#" method="POST" className="space-y-6">
+            <div>
+              <label htmlFor="usermed" className="block text-sm font-medium text-gray-900 text-left">
+  Nombre de usuario
+</label>
+<div className="mt-2">
+  <input 
+    id="usermed" 
+    type="text" 
+    name="usermed" 
+    required 
+    autoComplete="username" 
+    className="block w-full rounded-md bg-white px-3 py-1.5 text-base 
+               text-gray-900 outline-1 outline-gray-300 
+               placeholder:text-gray-400 focus:outline-2 
+               focus:outline-teal-600 sm:text-sm" 
+  />
+</div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium text-gray-900">
+                  Contraseña
+                </label>
+              </div>
+              <div className="mt-2">
+                <input 
+                  id="password" 
+                  type="password" 
+                  name="password" 
+                  required 
+                  autoComplete="current-password" 
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base 
+                             text-gray-900 outline-1 outline-gray-300 
+                             placeholder:text-gray-400 focus:outline-2 
+                             focus:outline-indigo-600 sm:text-sm" 
+                />
+              </div>
+            </div>
+
+            <div>
+              <button 
+                type="submit" 
+                className="flex w-full justify-center rounded-md bg-teal-600 
+                           px-3 py-1.5 text-sm font-semibold text-white shadow 
+                           hover:bg-teal-500 
+                           focus-visible:outline-2 focus-visible:outline-offset-2 
+                           focus-visible:outline-teal-600"
+              >
+                Ingresar
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            ¿Olvidaste tu contraseña?{" "}
+            <a href="#" className="font-semibold text-teal-600 hover:text-teal-500">
+              Contactar al soporte
+            </a>
+          </p>
+        </div>
       </div>
-      <h1>Tesis con backend en Flask y frontend en React + Vite</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
